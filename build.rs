96d7adfd80638ac31d6aa75fae94e3ced9fe43d8
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2025-02-04 12:37:04 +0800

    在 Windows 上默认以管理员权限运行

--- build.rs
+++ build.rs
@@ -6,8 +6,23 @@ fn main() {
             .build_file_cargo("icon.ico")
             .expect("无法生成图标");
 
-        winres::WindowsResource::new()
-            .set_icon(ico_path.to_string_lossy().as_ref())
+        let mut res = winres::WindowsResource::new();
+
+        res.set_manifest(
+            r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+</assembly>
+    "#,
+        );
+
+        res.set_icon(ico_path.to_string_lossy().as_ref())
             .compile()
             .unwrap();
     }
